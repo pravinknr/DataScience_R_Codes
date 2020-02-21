81c011db9cae98c4f4b5b3d02f94bc5c8bde8ee7
Pravin Konar <pravinknr@gmail.com>
2020-02-21 13:25:54 +0530

    Update Linear Regression.r

--- Practice/Linear Regression.r
+++ Practice/Linear Regression.r
@@ -9,6 +9,8 @@
 
 # modelname <- lm(y-Axis~X-Axis, data = DatasetName)
 
+#predictionName <- predict(modelname, newdata = data.frame(X-Axis = Value we want to predict for))
+
 #In the Below Example i have imported the Dataset named NewspaperData
 model <- lm(sunday~daily, data = NewspaperData) # here lm stands for linear model, Sunday is Y Axis data and Daily is X Axis data , data is the dataset that we want to use
 summary(model) #To get the summary of the data
@@ -24,3 +26,26 @@ pred1
 #Predict sunday circulations for 500 daily circulations
 pred2 <- predict(model, newdata = data.frame(daily=500))
 pred2
+
+#Lets predict the data for some more values
+
+library("lattice")
+#Visualization
+dotplot(NewspaperData$daily, main = "DotPlot for Daily circulations")
+
+dotplot(NewspaperData$sunday, main = "DotPlot for Sunday Circulations")
+
+boxplot(NewspaperData$daily, col = "blue")
+
+boxplot(NewspaperData$sunday, col = "cyan")
+
+#Lets predict the sunday circulations for (500,460,290,320) Daily Circulations
+
+newdata1 <- data.frame(daily = c(500,460,290,320))
+pred3 <- predict(model, newdata = newdata1)
+pred3
+
+pred4<- predict(model) #This will predict the Sunday Values for all the Daily values of the model
+
+finaldata1 <- data.frame(NewspaperData,pred4, "Errors" = NewspaperData$sunday - pred4) #this will subtract the predicted value with the Actual values of the dataset
+finaldata1
