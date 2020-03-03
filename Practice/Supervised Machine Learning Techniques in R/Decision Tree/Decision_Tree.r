5b88fb88174b80bdf47bffd62e82ddcd91afe6e9
Pravin Konar <pravinknr@gmail.com>
2020-03-03 13:23:49 +0530

    Create Decision_Tree.r

---
+++ Practice/Supervised Machine Learning Techniques in R/Decision Tree/Decision_Tree.r
@@ -0,0 +1,28 @@
+#Decision Tree
+#Irisdataset is available in the R dataset
+datasets::iris
+data("iris")
+
+install.packages("caret")
+install.packages("C50")
+
+library(caret)
+library(C50)
+
+inTraininglocal <- createDataPartition(iris$Species,p=.70,list = F) #.70 means 70% Partition
+training<- iris[inTraininglocal,]
+testing<- iris[-inTraininglocal,]
+
+#Model Building
+#Model should be built on Training Dataset
+model <- C5.0(training$Species~., data = training) #Trials - Boosting Parameter
+
+#Generate the Model Summary
+summary(model)
+
+#Predict for test data
+
+pred<- predict.C5.0(model,testing[,-5])
+a<- table(testing$Species,pred) #Crating a table with the testing$species values and predicted values
+sum(diag(a))/sum(a) #to find the Accuracy of the Model
+plot(model)
