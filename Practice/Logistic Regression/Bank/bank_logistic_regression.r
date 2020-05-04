ad53fed968e4589063b9e26e0316e8c9825f68e5
Pravin Konar <pravinknr@gmail.com>
2020-05-05 01:43:33 +0530

    Updated Logistic Regression

--- Practice/Logistic Regression/Bank/bank_logistic_regression.r
+++ Practice/Logistic Regression/Bank/bank_logistic_regression.r
@@ -1,9 +1,27 @@
 #Logistic Regression
+#Output variable -> y
 
 #Lets Import the dataset
 
 bank <- read.csv(file.choose(), sep = ";") #The dta in the file is seperated using Semi-Colons
 summary(bank)
+
+#Standard Deviation
+sd(bank$age)
+sd(bank$balance)
+sd(bank$duration)
+sd(bank$campaign)
+sd(bank$pdays)
+sd(bank$previous)
+
+#Variance
+var(bank$age)
+var(bank$balance)
+var(bank$duration)
+var(bank$campaign)
+var(bank$pdays)
+var(bank$previous)
+
 #We see that the categorical Variables are defined as Character. Lets convert it into the Categorical variable using Factor function
 
 bank$job <- as.factor(bank$job)
@@ -19,6 +37,8 @@ bank$y <- as.factor(bank$y)
 
 summary(bank)
 
+windows()
+pairs(bank)
 #Lets Build a generalized linear regression model
 
 bank_model <- glm(bank$y~. , data = bank, family = "binomial")
@@ -27,12 +47,38 @@ summary(bank_model)
 #Lets predict the values using the model
 
 pred <- predict(bank_model, bank, type = "response")
+#pred <- ifelse(pred>0.8,"yess","no")
+
+#The Coefficients
+round(exp(coef(bank_model)),3)
 
 #Lets build the Confusion Matrix
-confusion_matrix <- table(bank$y, pred >0.5)
+confusion_matrix <- table(bank$y, pred >0.8)
 confusion_matrix
 
 #Lets compute the Accuracy of the Model
 accuracy <- sum(diag(confusion_matrix))/sum(confusion_matrix)
 accuracy
 #The Accuracy of the model we built is 90.18%
+
+#Reciever Operating Characteristics
+install.packages("pROC")
+library(pROC)
+
+bank_roc <- roc(bank$y, pred)
+bank_roc
+
+#Lets see the Sensitivities
+sens <-bank_roc$sensitivities[2:45212]
+
+#Lets see the Specificity
+spec <-bank_roc$specificities[2:45212]
+
+bnk_roc_data <- data.frame(sens, spec)
+bnk_roc_data
+
+library(ggplot2)
+#Lets Plot the ROC curve with Specificity and Sensitivitty
+ggplot(bnk_roc_data, aes(x=spec, y=sens)) + geom_line()
+
+#Here the Area Under Curve(AUC) is 0.9079
--- Practice/Logistic Regression/Credit Card/creditcard_Logistic_Regression.r
+++ Practice/Logistic Regression/Credit Card/creditcard_Logistic_Regression.r
@@ -9,6 +9,29 @@ summary(creditcard)
 #We dont want the first column so we will remove it
 creditcard1 <- creditcard[,-1]
 
+#Standard Deviation
+sd(creditcard1$reports)
+sd(creditcard1$age)
+sd(creditcard1$income)
+sd(creditcard1$share)
+sd(creditcard1$expenditure)
+sd(creditcard1$dependents)
+sd(creditcard1$months)
+sd(creditcard1$majorcards)
+sd(creditcard1$active)
+
+#Variance
+var(creditcard1$reports)
+var(creditcard1$age)
+var(creditcard1$income)
+var(creditcard1$share)
+var(creditcard1$expenditure)
+var(creditcard1$dependents)
+var(creditcard1$months)
+var(creditcard1$majorcards)
+var(creditcard1$active)
+
+#Lets convert the Categorical columns into Factors
 creditcard1$card <- as.factor(creditcard1$card)
 creditcard1$owner <- as.factor(creditcard1$owner)
 creditcard1$selfemp <- as.factor(creditcard1$selfemp)
@@ -20,7 +43,7 @@ sum(is.na(creditcard1)) #To see total Number of Null Value
 credit_model <- glm(creditcard1$card~. , data = creditcard1, family = "binomial")
 summary(credit_model)
 
-pred <- data.frame(predict(credit_model,creditcard1,type = "response"))
+pred <- predict(credit_model,type = "response")
 pred
 
 #lets make a confusion matrix
@@ -33,3 +56,25 @@ accuracy
 #The Accuracy of the Model is 85.97
 
 final <- data.frame(creditcard1$card, pred)
+
+#Reciever Operating Characteristics
+install.packages("pROC")
+library(pROC)
+
+creditcard_roc <- roc(creditcard1$card, pred)
+creditcard_roc
+
+#Lets see the Sensitivities
+sens <-creditcard_roc$sensitivities
+sens
+#Lets see the Specificity
+spec <-creditcard_roc$specificities
+
+credit_roc_data <- data.frame(sens, spec)
+credit_roc_data
+
+library(ggplot2)
+#Lets Plot the ROC curve with Specificity and Sensitivitty
+ggplot(credit_roc_data, aes(x=spec, y=sens))+ geom_line()
+
+#Here the Area Under Curve(AUC) is 0.6935
